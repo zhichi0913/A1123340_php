7932eebcd075f52b72bce525996c4f874ba68374
zhichi0913 <zx699292@gmail.com>
2025-03-13 16:46:44 +0800

    A1123340_HW2

---
+++ 0307/info1_0307.php
@@ -0,0 +1,64 @@
+<html>
+
+<head>
+    <title>
+        迎新活動行程與報名表單
+    </title>
+    <meta charset="utf-8">
+</head>
+
+<body>
+
+    <h1>報名表</h1>    
+<table border="1">
+
+<form action="info2_0307.php" method="GET">
+Please input your name:<input type="text" name="uName"><br>
+
+Please input your email:<input type="email" name="uEmail"><br>
+
+Please select your gender:Male<input type="radio" name="uGender" value="male">
+Female<input type="radio" name="uGender" value="female"><br>
+
+Please select your eating habit:
+Study<input type="checkbox" name="uHabit[]" value="meet">
+Sleep<input type="checkbox" name="uHabit[]" value="vegetable">
+Game<input type="checkbox" name="uHabit[]" value="seefood">
+
+<br><input type="submit"><input type="reset">
+
+</form>
+
+</table>
+
+    <h1>行程表</h1> 
+<table border="1" >
+    <tr>
+        <td></td>
+        <td>DAY1</td>
+        <td>DAY2</td>
+        <td>DAY3</td>
+    </tr>
+    <tr>
+        <td>早上</td>
+        <td>高雄大學集合出發</td>
+        <td>海灘遊戲</td>
+        <td>大地遊戲</td>
+    </tr>
+    <tr>
+        <td>下午</td>
+        <td>破冰遊戲</td>
+        <td>大地遊戲</td>
+        <td>頒獎</td>
+    </tr>
+    <tr>
+        <td>晚上</td>
+        <td>墾丁大街</td>
+        <td>營火晚會</td>
+        <td>回到高雄大學</td>
+    </tr>
+</table>
+
+</body>
+
+</html>
