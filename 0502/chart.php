28a2543e90c53e209c77b53a639518c0cd313072
zhichi0913 <zx699292@gmail.com>
2025-05-02 14:45:00 +0800

    add 0502

---
+++ 0502/chart.php
@@ -0,0 +1,52 @@
+<html>
+  <head>
+
+
+  <?php
+  require("db.inc");
+
+  $sql = "SELECT * FROM pie"; 
+
+mysqli_set_charset($link, 'utf8');
+   
+?>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day']
+          
+     <?php     
+      if ( $result = mysqli_query($link, $sql)){
+      while( $row = mysqli_fetch_assoc($result)){  
+        echo ",['".$row["name"]."',".$row["data"]."]";
+      }
+      }
+     ?>
+          
+    //      ,
+    //      ['Work',     11],
+    //      ['Eat',      2],
+    //      ['Commute',  2],
+    //      ['Watch TV', 2],
+    //      ['Sleep',    7]
+        ]);
+
+        var options = {
+          title: 'My Daily Activities'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
+  <body>
+    <div id="piechart" style="width: 900px; height: 500px;"></div>
+  </body>
+</html>
